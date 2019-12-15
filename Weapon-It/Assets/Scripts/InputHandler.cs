562a8a1e5d52ba48917e03df5f4c9d455e55f8d4
Bocha <Adiel12430@gmail.com>
2019-12-15 21:03:25 +0200

    Button dont get touch input

--- Weapon-It/Assets/Scripts/InputHandler.cs
+++ Weapon-It/Assets/Scripts/InputHandler.cs
@@ -13,7 +13,6 @@ public class InputHandler : MonoBehaviour
     }
     #endregion
 
-    public EventSystem eventSys;
 
     // Divide input by this to get location by the precentage.
     int screenWidth;
@@ -27,34 +26,65 @@ public class InputHandler : MonoBehaviour
         screenWidth = Screen.width;
         screenHeight = Screen.height;
 
-        Vector2 inputPrecentage = new Vector2(.5f, .5f);
+        inputPrecentage = new Vector2(.5f, .5f);
     }
 
     private void Update()
     {
 #if UNITY_EDITOR
         // Divide the position by the screen height to get precentage based location.
-        inputPrecentage = new Vector2(Input.mousePosition.x / screenWidth, Input.mousePosition.y / screenHeight);
 
-        // TEMPORARY - Clamp the movement.y to prevent the weapon from getting too high.
-        // Replace it later with a logarithmic function that moves the weapon less when getting too high
+        if (Input.GetMouseButton(0))
+        {
+            if (!CanGetTouchInput())
+                return;
 
-        inputPrecentage.y = Mathf.Clamp(inputPrecentage.y, .05f, .5f);
+            inputPrecentage =
+                new Vector2(Input.mousePosition.x / screenWidth, Input.mousePosition.y / screenHeight);
 
+            // TEMPORARY - Clamp the movement.y to prevent the weapon from getting too high.
+            // Replace it later with a logarithmic function that moves the weapon less when getting too high
+
+            inputPrecentage.y = Mathf.Clamp(inputPrecentage.y, .05f, .5f);
+        }
+        else
+            startedOnUi = false;
 #else
-        if(Input.touchCount != 0 && CanGetTouchInput()){
+        if (Input.touchCount != 0)
+        {
+            if (!CanGetTouchInput())
+                return;
+
             inputPrecentage = new Vector2(
               x: Input.GetTouch(0).position.x / screenWidth,
               y: Input.GetTouch(0).position.y / screenHeight);
         }
+        else
+            startedOnUi = false;
 #endif
     }
 
+    bool startedOnUi;
     bool CanGetTouchInput()
     {
-        if (!eventSys.IsPointerOverGameObject())
+#if UNITY_EDITOR
+        if (EventSystem.current.IsPointerOverGameObject() || startedOnUi)
+        {
+            startedOnUi = true;
+            Debug.Log("OnUI");
             return false;
+        }
 
         return true;
+#else
+         if(EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId) || startedOnUi)
+        {
+            startedOnUi = true;
+            Debug.Log("OnUI");
+            return false;
+        }
+
+        return true;
+#endif
     }
 }
